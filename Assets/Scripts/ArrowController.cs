29393a96adacb6e392a85faa6f18aebb8d8167e4
moenage <moenage7@yahoo.com>
2024-02-17 23:02:51 -0800

    Finished up final strcuture and added instructions

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -14,15 +14,15 @@ public class ArrowController : MonoBehaviour
 
     private bool correctOrder = false;
 
-    public int[] arrowOrder = { 0, 1, 3 };
+    public int[] arrowOrder = { 0, 1, 3, 2 };
 
-    private int[] currentOrder = { 4, 4, 4 };
+    private int[] currentOrder = { 4, 4, 4, 4 };
 
     private GameObject[] childrenArray;
 
-    private Renderer[] rendererArray = new Renderer[4];
+    private Renderer[] rendererArray = new Renderer[5];
 
-    private ArrowClicked[] clickedArray = new ArrowClicked[4];
+    private ArrowClicked[] clickedArray = new ArrowClicked[5];
 
     private int countOrder = 0;
 
@@ -72,7 +72,7 @@ public class ArrowController : MonoBehaviour
     }
 
     public void checkArrows(int arrowNum) {
-        if( countOrder < 3) {
+        if( countOrder < 4) {
             currentOrder[countOrder] = arrowNum;
 
             if (countOrder == 0) {
@@ -87,10 +87,14 @@ public class ArrowController : MonoBehaviour
                 rendererArray[arrowNum].material.color = Color.blue;
             }
 
+            else if (countOrder == 3) {
+                rendererArray[arrowNum].material.color = Color.black;
+            }
+
             countOrder++;
         }
 
-        if(countOrder >= 3) {
+        if(countOrder >= 4) {
             correctOrder = true;
             for (int i = 0; i < arrowOrder.Length; i++) {
                 if (arrowOrder[i] != currentOrder[i]) {
@@ -115,6 +119,8 @@ public class ArrowController : MonoBehaviour
     }
 
     IEnumerator WrongAnswer() {
+        countOrder = 0;
+
         // Play wrong answer sound
         if (audioSource != null ) {
             audioSource.PlayOneShot(wrongAnswerSound);
